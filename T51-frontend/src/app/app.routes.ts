ee70b95410f6523e3f05401f9ff299dba4824c9d
Fefemyster <kenennlara@gmail.com>
2025-12-09 00:17:55 -0600

    Creating components for sales part

--- T51-frontend/src/app/app.routes.ts
+++ T51-frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './component/login/login.component';
 import { UsersComponent } from './component/users/users.component';
 import { HomeComponent } from './component/home/home.component';
 import { ProductosComponent } from './component/productos/productos.component';
+import { VentasComponent } from './component/ventas/ventas.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'users', component: UsersComponent },
   { path: 'home', component: HomeComponent },
   { path: 'productos', component: ProductosComponent },
+  { path: 'ventas', component: VentasComponent },
 ];
